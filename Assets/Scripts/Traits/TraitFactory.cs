11514bd52e65d46199a493de17c2fc6377597acc
Chris John <Chris.P.John@gmail.com>
2016-08-27 00:02:26 -0700

    Implement breed fucntion, make trait members public, add trait list and first create function to factory.

--- Assets/Scripts/Traits/TraitFactory.cs
+++ Assets/Scripts/Traits/TraitFactory.cs
@@ -1,6 +1,64 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class TraitFactory {
+    Dictionary<string, BaseTrait> instantiatedTraits = new Dictionary<string, BaseTrait>();
 
+    enum Traits
+    {
+        Tiny,
+        Small,
+        Midsized,
+        Large,
+        Huge,
+        Enormous,
+        Strong,
+        Weak,
+        Quick,
+        Slow,
+        Aggressive,
+        Docile,
+        Flying,
+        KeenEyes,
+        Armored,
+        Spikes,
+        KeenSmell,
+        Horny,
+        Horned,
+        QuickBreeding,
+        SlowBreeding,
+        MultiOffspring,
+        Loyal,
+        Independent,
+    }
+
+    enum TraitClass
+    {
+        Size,
+    }
+
+    BaseTrait createLarge()
+    {
+        if (instantiatedTraits.ContainsKey(Traits.Large.ToString()))
+        {
+            return instantiatedTraits[Traits.Large.ToString()];
+        }
+        BaseTrait large = new BaseTrait();
+        large.fightScore = 1;
+        large.foodScore = 3;
+        large.strengthScore = 1;
+        large.trackingScore = 0;
+
+        large.inheritanceChance = 0.5f;
+
+        large.name = Traits.Large.ToString();
+        large.type = Traits.Large.ToString();
+        large.traitClass = TraitClass.Size.ToString();
+
+        large.linkageMap.Add(Traits.Strong.ToString(), 0.2f);
+        large.linkageMap.Add(Traits.Weak.ToString(), -0.2f);
+        large.linkageMap.Add(Traits.SlowBreeding.ToString(), 0.2f);
+        large.linkageMap.Add(Traits.Slow.ToString(), 0.2f);
+        return large;
+    }
 }
