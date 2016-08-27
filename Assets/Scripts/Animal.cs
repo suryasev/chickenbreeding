c443435d9d67caca988af611fe86c94a4b56573b
Chris John <Chris.P.John@gmail.com>
2016-08-26 23:07:02 -0700

    Initial checkin of animals and traits.

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+
+    List<BaseTrait> trait = new List<BaseTrait>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
